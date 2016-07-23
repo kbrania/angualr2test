464c93ba42dd07391bfd5d4fce4b6f0dbc873dd4
sandeep_barla <sandeepreddybarla@gmail.com>
2016-07-23 16:23:12 +0530

    init commit

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by brtindia on 23-07-2016.
+ */
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App, Hello</h1>'
+})
+
+export class AppComponent { }
---
+++ app/main.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by brtindia on 23-07-2016.
+ */
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component';
+bootstrap(AppComponent);
